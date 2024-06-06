88f7f17dd36c7e44d3f1e887500262a0f63cca45
Sebastian Amaral <56357599+SHA-Arg@users.noreply.github.com>
2024-06-06 03:57:24 -0300

    update

--- Programacion I/REFERENCIAS/FUNCIONES/Orden Superior/high_order_func.py
+++ Programacion I/REFERENCIAS/FUNCIONES/Orden Superior/high_order_func.py
@@ -5,6 +5,7 @@
 # Ejemplo de map con funcion lambda:
 
 from functools import reduce
+
 numeros = [1, 2, 3, 4, 5]
 cuadrados = list(map(lambda x: x**2, numeros))
 print(cuadrados)
@@ -23,7 +24,7 @@ numeros = [1, 2, 3, 4, 5]
 pares = list(filter(lambda x: x % 2 == 0, numeros))
 print(pares)
 
-# En este ejemplo, la funcion lambda se utiliza con la funcion filter para filtrar los numeros pares de la lista de numeros. El resultado es una nueva lista con los numeros pares.
+# En este ejemplo, la funcion lambda se utiliza con la funcion filter para filtrar los numeros pares de la lista de numeros. El resultado es una nueva lista con los numeros pares. En este caso, el resultado es [2, 4].
 
 lista_edades = [20, 25, 30, 35, 40]
 mayores_de_30 = list(filter(lambda x: x > 30, lista_edades))
@@ -38,4 +39,19 @@ numeros = [1, 2, 3, 4, 5]
 suma = reduce(lambda x, y: x + y, numeros)
 print(suma)
 
-# En este ejemplo, la funcion lambda se utiliza con la funcion reduce para sumar todos los elementos de la lista de numeros. El resultado es la suma de todos los numeros.
+# En este ejemplo, la funcion lambda se utiliza con la funcion reduce para sumar todos los elementos de la lista de numeros. El resultado es la suma de todos los numeros. En este caso, el resultado es 15.
+
+# sorted con funcion lambda
+
+lista_estudiantes = [('Juan', 90),
+                     ('Maria', 85),
+                     ('Pedro', 88),
+                     ('Ana', 92)]
+
+lista_ordenada = sorted(
+    lista_estudiantes, key=lambda estudiante: estudiante[1], reverse=True)
+# otra forma
+# lista_ordenada = sorted(lista_estudiantes, key=lambda x: x[1], reverse=True)
+print(lista_ordenada)
+
+# En este ejemplo, la funcion lambda se utiliza como argumento de la funcion sorted para ordenar la lista de estudiantes por nota de forma descendente. La funcion lambda toma cada estudiante como argumento y devuelve su nota, que se utiliza como criterio de ordenamiento. El resultado es una lista de estudiantes ordenada por nota de forma descendente.
